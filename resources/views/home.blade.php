adb9f71fd69395beb7ca933b7b890cd50e3a03da
Amaury VMO <amauryvmo@gmail.com>
2019-08-17 19:33:58 -0300

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             @foreach($products as $product)
                 <div class="col-sm-3">
                     <div class="card">
-                        <img src="https://i0.wp.com/marketingcomcafe.com.br/wp-content/uploads/2017/12/banco-imagens-gratis.png?zoom=2&resize=720%2C445&ssl=1" class="card-img-top" alt="...">
+                        <img src="{{ $product->image }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{ $product->name }}</h5>
                             <p class="card-text">{{ $product->short_description }}</p>
